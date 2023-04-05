71143c1bdc21f7c45fd701c4e40d93d472e37696
irutupatel <rpate347@illinois.edu>
2023-04-05 13:47:34 -0700

    Passing data with bindings: Add a theme picker

---
+++ Scrumdinger/ThemePicker.swift
@@ -0,0 +1,28 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Rutu Patel on 4/5/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
