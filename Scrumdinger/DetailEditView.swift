9a9bc157cc94354ad39e9ec10576e1fb0a706e67
irutupatel <rpate347@illinois.edu>
2023-04-05 11:15:02 -0700

    Creating the edit view: Add an edit view for scrum details

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,34 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Rutu Patel on 4/5/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var data = DailyScrum.Data()
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $data.title)
+                HStack {
+                    Slider(value: $data.lengthInMinutes, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(data.lengthInMinutes)) minutes")
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
