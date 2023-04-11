133a0f611b13337738ead277e66dc3dbbc49dae3
irutupatel <rpate347@illinois.edu>
2023-04-11 16:20:12 -0700

    Persisting data: Load data on app launch

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 
 class ScrumStore: ObservableObject {
-    @Published var scrum: [DailyScrum] = []
+    @Published var scrums: [DailyScrum] = []
     
     private static func fileURL() throws -> URL {
         try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
