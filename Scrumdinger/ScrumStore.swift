ce5831e9e3cacd18dbe4b88fa8940724345c9c92
irutupatel <rpate347@illinois.edu>
2023-04-11 15:50:22 -0700

    Persisting data: Add a method to load data

--- Scrumdinger/ScrumStore.swift
+++ Scrumdinger/ScrumStore.swift
@@ -15,4 +15,26 @@ class ScrumStore: ObservableObject {
         try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
             .appendingPathComponent("scrum.data")
     }
+    
+    static func load(completion: @escaping (Result<[DailyScrum], Error>)->Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let fileURL = try fileURL()
+                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
+                    DispatchQueue.main.sync {
+                        completion(.success([]))
+                    }
+                    return
+                }
+                let dailyScrum = try JSONDecoder().decode([DailyScrum].self, from: file.availableData)
+                DispatchQueue.main.sync {
+                    completion(.success(dailyScrum))
+                }
+            } catch {
+                DispatchQueue.main.sync {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
 }
